07292413c7736994ade304d43427023c589193c3
Egor <egor.shelemetev228@mail.ru>
2024-07-24 20:09:12 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+import Swiper from "swiper";
+
+import 'swiper/css'
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
