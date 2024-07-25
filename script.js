e630881171401d02cfc82bc6b61b27dedf264e66
Egor <egor.shelemetev228@mail.ru>
2024-07-25 17:29:11 +1000

    fix

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-import Swiper from "swiper";
 
-import 'swiper/css'
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
-
+window.addEventListener('scroll', (event) => {
+  let posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  if (posTop >= 396) {
+    document.querySelector('.header').style.position = 'relative'
+    document.querySelector('.dropdown-menu').classList.remove('show')
+  } else {
+    document.querySelector('.header').style.position = 'fixed'
+  }
+})
